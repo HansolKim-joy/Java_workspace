add8b0d256b49ffda2bd28c226e0f93093560051
HansolKim-joy <60562021+HansolKim-joy@users.noreply.github.com>
2020-02-02 13:16:59 +0900

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,12 @@
+package com.kh.first;
+
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("Hello World"); //출력하는 문장
+		// 한 줄 주석 
+		/* 
+		 * 여러줄의 주석이 생김
+		 오늘은 첫 수업이에요 :)
+		 */
+	}
+}
