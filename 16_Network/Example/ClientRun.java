e9107c7f8161d8c3312bdeaa9cda409e0dd639cd
HansolKim-joy <60562021+HansolKim-joy@users.noreply.github.com>
2020-02-02 16:47:39 +0900

    Add files via upload

---
+++ 16_Network/Example/ClientRun.java
@@ -0,0 +1,9 @@
+package com.kh.example.chap02_sendMsg.run;
+
+import com.kh.example.chap02_sendMsg.contoller.Client;
+
+public class ClientRun {
+	public static void main(String[] args) {
+		new Client().clientStart();
+	}
+}
---
+++ 16_Network/Example/ServerRun.java
@@ -0,0 +1,9 @@
+package com.kh.example.chap02_sendMsg.run;
+
+import com.kh.example.chap02_sendMsg.contoller.Server;
+
+public class ServerRun {
+	public static void main(String[] args) {
+		new Server().serverStart();
+	}
+}
