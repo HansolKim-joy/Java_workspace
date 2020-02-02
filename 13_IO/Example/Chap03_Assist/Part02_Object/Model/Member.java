4c800779e8b03bd9f18b9bda2332b59ec27c8b92
HansolKim-joy <60562021+HansolKim-joy@users.noreply.github.com>
2020-02-02 16:09:02 +0900

    Add files via upload

---
+++ 13_IO/Example/Chap03_Assist/Part02_Object/Model/Member.java
@@ -0,0 +1,103 @@
+package com.kh.example.chap03_assist.part02_object.model.vo;
+
+import java.io.Serializable;
+
+public class Member implements Serializable{
+	// - userId : String
+	// - userPwd : String
+	// - userName : String
+	// - email : String
+	// - age : int
+	// - gender : char
+	// - point : double
+
+	// + Member()
+	// + Member(String userId, String userPwd, String userName, String email, int age, char gender, double point)
+	// + getter/ setter
+	// + toString() : String
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2381941613205056360L;
+	private String userId;
+	private transient String userPwd; // transient : 직렬화에서 제외시킴 => 데이터가 빠지고 원래 기본 값이 null 값으로 보여짐
+	private String userName;
+	private String email;
+	private int age;
+	private char gender;
+	private double point;
+	
+	public Member() {}
+	public Member(String userId, String userPwd, String userName, String email, int age, char gender, double point) {
+		this.userId = userId;
+		this.userPwd = userPwd;
+		this.userName = userName;
+		this.email = email;
+		this.age = age;
+		this.gender = gender;
+		this.point = point;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	public String getUserPwd() {
+		return userPwd;
+	}
+	
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public char getGender() {
+		return gender;
+	}
+	
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	
+	public double getPoint() {
+		return point;
+	}
+	
+	public void setPoint(double point) {
+		this.point = point;
+	}
+	
+	@Override
+	public String toString() {
+		return "Member [userId= " + userId + ", userPwd=" + userPwd + ", userName=" + userName
+				+ ", email=" + email + ", age=" + age + ", gender=" + gender + ", point=" + point;
+	}
+}
