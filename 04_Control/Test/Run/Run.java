5e72f6b2adbb946714fd767d22d4e5908ba1d50a
HansolKim-joy <60562021+HansolKim-joy@users.noreply.github.com>
2020-02-02 14:18:23 +0900

    Rename 04_Control/Test/Run.java to 04_Control/Test/Run/Run.java

--- 04_Control/Test/Run/Run.java
+++ 04_Control/Test/Run/Run.java
@@ -4,10 +4,10 @@ import com.kh.test.MethodPrinter;
 
 public class Run {
 	public static void main(String[] args) {
-		System.out.println("main�޼ҵ� ����");
+		System.out.println("main¸Þ¼Òµå ½ÇÇà");
 		MethodPrinter mp = new MethodPrinter();
 		mp.methodA();
-		System.out.println("main�޼ҵ� ��");
+		System.out.println("main¸Þ¼Òµå ³¡");
 	}
 
 }
